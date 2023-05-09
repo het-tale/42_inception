f1c911ec7acb8390c78bd1ce164b0e52d9901c8d
het-tale <ettalebyhasnaa99@gmail.com>
2023-05-09 13:50:39 +0100

    the website doesn't work

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# sed -i "s/bind-address.*/bind-address=${ip_all}/" /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -i "s/#port.*/port=${maria_port}/" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+#-i :  modify the file in place
+#s/MY_VARIABLE=.*/MY_VARIABLE=${new_value}/     : matches any line in the file that starts with MY_VARIABLE= and replaces the entire line with MY_VARIABLE=new_value.
+
+service mysql start
+
+touch db
+
+    echo "CREATE DATABASE $db_name;" >> db
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_passwd';" >> db
+    echo "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_passwd';" >> db
+    echo "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%';" >> db
+    echo "FLUSH PRIVILEGES;" >> db
+    echo "EXIT" >> db
+
+    mysql < db
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+#The reason why you need to kill the MySQL PID before running mysqld_safe
+#is that mysqld_safe is a script that starts the MySQL server in the foreground
+# and monitors it for errors. If the server encounters an error or crashes,
+#mysqld_safe will attempt to restart it automatically.
+#if there is already a running instance of the MySQL server (as indicated by the PID file),
+#running mysqld_safe will likely fail, because it cannot start a new instance of the server while there is already one running.
+
+#The *.* wildcard notation implies that the user has permission to execute any task on any database in the database server.
+
+# /usr/bin/mysqld_safe
+mysqld
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+export name=$(ls /etc/php/)
+
+cd /var/www/html
+
+if [ -f './wp-config-sample.php' ]; then
+
+    mv wp-config-sample.php wp-config.php > /dev/null
+
+    chmod 777 wp-config.php
+
+    sed -i "s/database_name_here/$db_name/1" wp-config.php
+    sed -i "s/password_here/$db_passwd/1" wp-config.php
+    sed -i "s/username_here/$db_user/1" wp-config.php
+    sed -i "s/localhost/mariadb/1" wp-config.php
+
+    wp core install --title=$wp_title \
+                    --admin_user=$wp_admin \
+                    --admin_password=$wp_admin_psswd \
+                    --admin_email=$wp_admin_email \
+                    --url=$db_host \
+                    --allow-root
+
+    wp user create $wp_user $wp_email --role=author --user_pass=$wp_psswd --allow-root
+    sed -i "s/listen = \/run\/php\/php$name-fpm.sock/listen = 9000/1" /etc/php/$name/fpm/pool.d/www.conf
+
+    sed -i '60i\
+    define( 'WP_REDIS_HOST', 'redis' );\
+    define( 'WP_REDIS_PORT', 6379 );\
+    define('WP_CACHE', true);' wp-config.php
+
+fi
+
+wp plugin update --all --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp theme install twentysixteen --activate --allow-root
+
+wp redis enable --allow-root
+# the /run/php used by PHP-FPM to store process Id.
+mkdir -p /run/php
+
+
+# exec destroyed The current shell process , and entirely replace it by the command php-fpm
+exec php-fpm$name -F
