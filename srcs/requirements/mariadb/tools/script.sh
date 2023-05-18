98502c31d331eb3cf1771fe17fead8276b579d6d
het-tale <ettalebyhasnaa99@gmail.com>
2023-05-18 21:42:50 +0100

    Test succeed

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# sed -i "s/bind-address.*/bind-address            = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
-# sed -i "s/#port.*/port                    = 3306/" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# source .env
-
 service mysql start
 
 touch db
@@ -19,5 +14,3 @@ touch db
     mysql -h localhost -uroot -p$DB_ROOT_PASS < db
 
 kill $(cat /var/run/mysqld/mysqld.pid)
-
-# mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,57 +1,14 @@
 #!/bin/bash
 
-# source .env
-
-# mkdir -p /var/www/html > /dev/null
-
-# chown -R www-data:www-data /var/www/html
-
-# cd /var/www/html
-
 wp core download --allow-root
-
-
-# mv wp-config-sample.php wp-config.php > /dev/null
-
-#     chmod 777 wp-config.php
-
-#     sed -i "s/database_name_here/$MYSQL_DATABASE/1" wp-config.php
-#     sed -i "s/password_here/$DB_PASS/1" wp-config.php
-#     sed -i "s/username_here/$DB_USER/1" wp-config.php
-#     sed -i "s/localhost/mariadb/1" wp-config.php
-
-wp core config --dbhost=$HOST --dbname=$MYSQL_DATABASE --dbuser=$DB_USER --dbpass=$DB_PASS --allow-root
-
-    # wp core install --title=random_hdchy \
-    #                 --admin_user=asilo99 \
-    #                 --admin_password=asilo99 \
-    #                 --admin_email=ettalebyhasnaa99@gmail.com \
-    #                 --url=het-tale.42.fr \
-    #                 --allow-root
-
-# wp config create --dbname=$MYSQL_DATABASE \
-#     --dbuser=$DB_USER --dbpass=$DB_PASS \
-#     --dbhost=$HOST --allow-root
-
-echo HIIIII
-
+mv wp-config-sample.php wp-config.php > /dev/null
+chmod 777 wp-config.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/1" wp-config.php
+sed -i "s/password_here/$DB_PASS/1" wp-config.php
+sed -i "s/username_here/$DB_USER/1" wp-config.php
+sed -i "s/localhost/mariadb/1" wp-config.php
 wp core install --url=$DB_HOST --title=$WP_TITLE --admin_user=$WP_ADMN_USR --admin_password=$WP_ADMN_PASS  --admin_email=$WP_ADMN_MAIL --allow-root
+wp user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PASS --allow-root
+wp theme install twentysixteen --activate --allow-root
 
-echo HIIIII
-
-    # wp user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PASS --allow-root
-    
-
-
-
-# wp plugin update --all --allow-root
-# wp theme install twentysixteen --activate --allow-root
-# wp theme install twentytwentytwo --activate --allow-root
-
-
-# chown -R www-data:www-data /run/php
-
-
-# exec destroyed The current shell process , and entirely replace it by the command php-fpm
-# exec php-fpm$name -F
 exec $@
